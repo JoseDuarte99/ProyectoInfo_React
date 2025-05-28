029fd5fa6a7807409f77b00d02b51d526c3f37d7
JoseDuarte <estebnjose@gmail.com>
2025-05-28 17:44:36 -0300

    Commit from the desktop computer

--- ProjectInfo/src/App.tsx
+++ ProjectInfo/src/App.tsx
@@ -1,67 +1,60 @@
-import { ProductCard, TitleSection, Section} from "./components"
-import imgSrc from "./assets/OIP.jpg";
+// import { ProductCard, TitleSection, Section, Navbar} from "./components"
+import {Navbar} from "./components"
+// import imgSrc from "./assets/OIP.jpg";
 
 function App() {
   return (
     <>
-    <Section titleSection={<TitleSection title="Más vendidos de la semana en Tecnología" anchor="Ir a Más vendidos" />}>
-    <ProductCard 
-    img={imgSrc}
-    title="Auriculares Bluetooth"
-    previousPrice={9999}
-    price={5999} 
-    priceInfo="Descuento exclusivo" 
-    shippingInfo="Envío gratis" 
-    />
-    <ProductCard 
-    img={imgSrc}
-    title="Auriculares Bluetooth"
-    previousPrice={9999}
-    price={5999} 
-    priceInfo="Descuento exclusivo" 
-    shippingInfo="Envío gratis" 
-    />
-    <ProductCard 
-    img={imgSrc}
-    title="Auriculares Bluetooth"
-    previousPrice={9999}
-    price={5999} 
-    priceInfo="Descuento exclusivo" 
-    shippingInfo="Envío gratis" 
-    />
-    <ProductCard 
-    img={imgSrc}
-    title="Auriculares Bluetooth"
-    previousPrice={9999}
-    price={5999} 
-    priceInfo="Descuento exclusivo" 
-    shippingInfo="Envío gratis" 
-    />
-    <ProductCard 
-    img={imgSrc}
-    title="Auriculares Bluetooth"
-    previousPrice={9999}
-    price={5999} 
-    priceInfo="Descuento exclusivo" 
-    shippingInfo="Envío gratis" 
-    />
-    <ProductCard 
-    img={imgSrc}
-    title="Auriculares Bluetooth"
-    previousPrice={9999}
-    price={5999} 
-    priceInfo="Descuento exclusivo" 
-    shippingInfo="Envío gratis" 
-    />
-    <ProductCard 
-    img={imgSrc}
-    title="Auriculares Bluetooth"
-    previousPrice={9999}
-    price={5999} 
-    priceInfo="Descuento exclusivo" 
-    shippingInfo="Envío gratis" 
-    />
-    </Section>
+    {/* <Section titleSection={<TitleSection title="Más vendidos de la semana en Tecnología" link="Ir a Más vendidos" />}>
+      <ProductCard 
+      img={imgSrc}
+      title="Auriculares Bluetooth"
+      previousPrice={9999}
+      price={5999} 
+      shippingInfo="Envío gratis" 
+      />
+      <ProductCard 
+      img={imgSrc}
+      title="Auriculares Bluetooth"
+      previousPrice={9999}
+      price={5999} 
+      priceInfo="Descuento exclusivo" 
+      shippingInfo="Envío gratis" 
+      />
+      <ProductCard 
+      img={imgSrc}
+      title="Auriculares Bluetooth"
+      previousPrice={9999}
+      price={5999} 
+      priceInfo="Descuento exclusivo" 
+      />
+      <ProductCard 
+      img={imgSrc}
+      title="Auriculares Bluetooth"
+      price={5999} 
+      priceInfo="Descuento exclusivo" 
+      shippingInfo="Envío gratis" 
+      />
+      <ProductCard 
+      img={imgSrc}
+      title="Auriculares Bluetooth"
+      previousPrice={9999}
+      price={5999} 
+      priceInfo="Descuento exclusivo" 
+      />
+      <ProductCard 
+      img={imgSrc}
+      title="Auriculares Bluetooth"
+      previousPrice={9999}
+      price={5999} 
+      priceInfo="Descuento exclusivo" 
+      shippingInfo="Envío gratis" 
+      />
+    </Section> */}
+
+
+    <Navbar />
+
     </>
   )
 }
--- ProjectInfo/src/components.tsx
+++ ProjectInfo/src/components.tsx
@@ -1,20 +1,26 @@
+import imgIcon from "./assets/icon.jpeg";
+
 // TIPADO DE PRODUCT-CARD
 type ProductCardProps = {
     img: string;
     title: string;
-    previousPrice: number;
+    previousPrice?: number;
     price: number;
-    priceInfo: string;
-    shippingInfo: string;
+    priceInfo?: string;
+    shippingInfo?: string;
 };
 
 // TIPADO DE TITLE-SECTION
 type TitleSectionProps = {
     title: string;
-    anchor?: string;
+    link?: string;
 };
 
-
+// TIPADO DE SECTION
+type SectionProps = {
+    titleSection: React.ReactNode;
+    children: React.ReactNode;
+};
 
 // COMPONENTE PRODUCT-CARD
 export function ProductCard(props: ProductCardProps) {
@@ -22,50 +28,62 @@ export function ProductCard(props: ProductCardProps) {
     
     return (
         <div className=" pl-2 pr-2 pb-2">
-            <img width={250} height={250} src={img} alt={title} />
-            <h3 className="text-neutral-700 text-left text-medium mb-4">
-                {title}
-            </h3>
-            <p className="text-neutral-500 text-left text-xs line-through">
-                ${previousPrice.toLocaleString('es-AR')}
-            </p>
-            <h4 className="text-black text-left text-2xl font-medium">
-                ${price.toLocaleString('es-AR')}
-            </h4>
-            <p className="text-green-700 text-left text-xs font-normal mb-2 mt-2">
-                {priceInfo}
-            </p>
-            <p className="text-green-700 text-left text-xs font-bold ">
-                {shippingInfo}
-            </p>
+            <a href="#">
+                <img width={250} height={250} src={img} alt={title} />
+                <h3 className="text-neutral-700 text-left text-medium mb-4">
+                    {title}
+                </h3>
+                <p className="text-neutral-500 text-left text-xs line-through">
+                    {previousPrice !== undefined ? `$${previousPrice}` : ""}
+                </p>
+                <h4 className="text-black text-left text-2xl font-medium">
+                    ${price.toLocaleString('es-AR')}
+                </h4>
+                <p className="text-green-700 text-left text-xs font-normal mb-2 mt-2">
+                    {priceInfo}
+                </p>
+                <p className="text-green-700 text-left text-xs font-bold ">
+                    {shippingInfo}
+                </p>
+            </a>
         </div>
     );
 };
 
-
 // COMPONENTE TITLE-SECTION
 export function TitleSection(props: TitleSectionProps) {
-    const {title, anchor} = props
+    const {title, link} = props
 
     return (
         <div className="pl-2">
             <h2  className="text-black text-left text-3xl font-medium pt-1">
-                {title}<a href="#" className="text-sky-800 text-left text-sm pl-4">{anchor}</a>
+                {title}<a href="#" className="text-red-600 text-left text-sm pl-4">{link}</a>
             </h2>
         </div>
     );
 };
 
 // COMPONENTE SECTION
-export function Section({ titleSection, children }: { titleSection: React.ReactNode; children: React.ReactNode }) {
+export function Section(props: SectionProps) {
+    const { titleSection, children} = props
     return (
         <div className="bg-white p-2 m-10 rounded-lg shadow-gray-300 shadow-lg ">
-            <>
-                {titleSection}
-            </>
+            <>{titleSection}</>
             <div className="flex">
                 {children}
             </div>
         </div>
+    );
+};
+
+
+// COMPONETE NAVBAR
+export function Navbar(){
+
+    return(
+        <div className="w-screen bg-yellow-300 ">
+            <img src={imgIcon} alt="" />
+        </div>
+
     );
 };
